a8ef4fdb08db2e063694e3eff91c802e285b1b21
Luan Orlandi <orlandi.luan@gmail.com>
2017-10-28 23:52:35 -0200

    Remove execute script

--- background.js
+++ background.js
@@ -1,10 +1,6 @@
 var audio = new Audio();
 audio.src = "audio/erro1.mp3";
 
-chrome.tabs.onCreated.addListener(function() {
-  chrome.tabs.executeScript(null, {file: "content.js"});
-});
-
 chrome.runtime.onMessage.addListener(function(message) {
     if(message == "play") {
         audio.play();
